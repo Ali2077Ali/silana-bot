5e114a342403655ac9e8a06e267e536e6f9f3a16
Ali2077Ali <basbh1234@gmail.com>
2024-09-30 02:38:30 +0300

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -3,14 +3,14 @@ let handler = async (m, { conn }) => {
   let caption = `
 *「 معلومات عن صاحب البوت 」*
 
-*Number :*\nwa.me/212717457920
-*instagram:*\ninstagram.com/noureddine_ouafy
+*Number :*\nwa.me/967781974099
+*instagram:*\ninstagram.com/noureddine_ouaf
 
-*youtube:*\nyoutube.com/@noureddineouafy2
+*youtube:*\nyoutube.com/@noureddineouafy
 
-*facebook page:*\nwww.facebook.com/profile.php?id=100063533185520
+*facebook page:*\nwww.facebook.com/profile.ph?id=100063533185520
 
-*script bot :* github.com/noureddineouafy
+*script bot :* github.com/noureddineouaf
 
 `.trim()
   m.reply(caption)
